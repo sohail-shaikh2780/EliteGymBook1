5767dee280dc75e97c04c90350fcf45219d430f0
Nikhil Kadam <83809232nk@gmail.com>
2024-08-09 16:32:58 +0530

    header modification with media

--- gymservices/src/Components/HeaderComponent.js
+++ gymservices/src/Components/HeaderComponent.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import "./IndexComponent.css";
+import React,{useState} from 'react';
+import "./header.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+// eslint-disable-next-line no-unused-vars
+//import Contact from "./Contact";
+//import { Link } from "react-router-dom";
+
 
 function HeaderComponent(){
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
     return(
     <header>
-        <nav>
-            <div class="logo">EliteGymBook</div>
+        <nav className="main-nav">
+            <div class="logo">
+                <h2>
+                    <span>E</span>lite
+                    <span>G</span>ymBook
+                </h2>
+            </div>
+            <div className= {showMediaIcons ? "menu-link mobile-menu-link" : 'menu-link'}>
             <ul>
-                <li class='ulist'><a href="#home">Home</a></li>
-                <li class='ulist'><a href="#about">About Us</a></li>
-                <li class='ulist'><a href="#contact" target='self'>Contact</a></li>
-                <li class='ulist'><a href="#login">LogIn</a></li>
-                <div class='signup'>
-                <button id="signup">SignUP</button>
-                </div>  
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+               
+                <li><a href='#contact' target='self'>Contact</a></li>
+                <li><a href="#login">LogIn</a></li>           
             </ul>
+            </div>
+            
+            <button id='signup'>SignUP</button> 
+            
+            {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+          
         </nav>
+        
     </header>
     )
 }
